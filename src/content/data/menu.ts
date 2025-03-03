6c977fc5a3922039b3ac09c009dcd88b11278575
mohansky <38560439+mohansky@users.noreply.github.com>
2025-03-03 09:27:11 +0530

    Delete src directory

--- src/content/data/menu.ts
+++
@@ -1,9 +0,0 @@
-import type { MenuLink } from "@/types";
-
-export const links: MenuLink[] = [
-    { text: "About", link: "/#about" },
-    { text: "Rooms", link: "/#rooms" },
-    { text: "Facilities", link: "/#facilities" },
-    { text: "Directions", link: "/#directions" },
-    { text: "Contact", link: "/#contact" },
-  ];
